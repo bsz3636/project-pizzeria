8933973b1337fd36eb3e45a4dcddda41982c08ef
Beata Karwowska <bsz3636@o2.pl>
2021-12-07 20:31:29 +0100

    add image support

--- src/js/script.js
+++ src/js/script.js
@@ -95,6 +95,8 @@
       //console.log('cartButton: ',this.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       //console.log('priceElem: ', this.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector (select.menuProduct.imageWrapper);
+      console.log('imageWrapper: ',this.imageWrapper);
     }
 
     initAccordion(){
@@ -111,7 +113,7 @@
 
         /*find active product (product that has active class) */
         const activeProduct = document.querySelector(select.all.menuProductsActive);
-        console.log('activeProduct',activeProduct);
+        //console.log('activeProduct',activeProduct);
         
         /*if there is active product and it's not thisProduct.element, remove class active from it */
         if(activeProduct && activeProduct != thisProduct.element){
@@ -151,7 +153,7 @@
 
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
+      //console.log('formData', formData);
       
       // set price to default price
       let price = thisProduct.data.price;
@@ -161,16 +163,17 @@
       
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log('parm: ',paramId, param);
+        //console.log('parm: ',paramId, param);
 
         // for every option in this category
         for(let optionId in param.options) {
       
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log('option:' ,optionId, option);
+          //console.log('option:' ,optionId, option);
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          if(optionSelected) {
             // check if the option is not default
             if(!option.default===true ) {
             // add option price to price variable
@@ -183,7 +186,18 @@
               price-=option.price;
             }
           }
-
+          /*find image Znalezienie obrazka o klasie .paramId-optionId w divie z obrazkami.*/
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          console.log('optionImage: ',optionImage);
+          if (optionImage) {
+            if(optionSelected){
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
+          }
+          //thisProduct.imageWrapper = thisProduct.element.querySelector (select.menuProduct.imageWrapper);
+          // console.log('imageWrapper: ',this.imageWrapper);
         }
       }
 
@@ -195,7 +209,7 @@
   const app = {
     initMenu : function () {
       const thisApp = this;
-      console.log('thisApp.data: ',thisApp.data);
+      //console.log('thisApp.data: ',thisApp.data);
       for(let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);
       }
@@ -209,11 +223,11 @@
     },
     init: function(){
       const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
+      //console.log('*** App starting ***');
+      //console.log('thisApp:', thisApp);
+      //console.log('classNames:', classNames);
+      //console.log('settings:', settings);
+      //console.log('templates:', templates);
 
       thisApp.initData();
       
