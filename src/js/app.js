1608901ac400b10b4dabd5d1789e11d9ac5f2035
Beata Karwowska <bsz3636@o2.pl>
2022-01-23 18:38:16 +0100

    add subpages - problem

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,68 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+    
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+    
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /*get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /*run thisApp.activatePage with that id*/
+        thisApp.activatePage(id);
+
+        /*change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /*add class "active" to matching pages, remove from non-mathing */
+    for(let page of thisApp.pages){
+      /*if(page.id == pageId){
+        page.classList.add(classNames.pages.active);
+      } else {
+        page.classList.remove(classNames.pages.active);
+      }*/
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+
+    }
+    
+    /*add class "active" to matching links, remove from non-mathing */
+    for(let link of thisApp.navLinks){
+      
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initMenu : function () {
     const thisApp = this;
     //console.log('thisApp.data: ',thisApp.data);
@@ -33,19 +93,6 @@ const app = {
     console.log('thisApp.data: ',JSON.stringify(thisApp.data));
   },
 
-  init: function(){
-    const thisApp = this;
-    //console.log('*** App starting ***');
-    //console.log('thisApp:', thisApp);
-    //console.log('classNames:', classNames);
-    //console.log('settings:', settings);
-    //console.log('templates:', templates);
-
-    thisApp.initData();
-      
-    thisApp.initCart();
-  },
-    
   initCart: function(){
     const thisApp = this;
 
@@ -55,8 +102,21 @@ const app = {
     thisApp.productList = document.querySelector(select.containerOf.menu);
 
     thisApp.productList.addEventListener('add-to-cart', function(event){
-      app.cart.add(event.detail.product.prepareCartProduct());
+      app.cart.add(event.detail.product);
     });
+  },
+
+  init: function(){
+    const thisApp = this;
+    //console.log('*** App starting ***');
+    //console.log('thisApp:', thisApp);
+    //console.log('classNames:', classNames);
+    //console.log('settings:', settings);
+    //console.log('templates:', templates);
+    
+    thisApp.initPages();
+    thisApp.initData();
+    thisApp.initCart();
   },
 };
   
